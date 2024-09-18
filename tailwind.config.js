80a09f799c3eeec21757dd5dbe5e0c70ed05e2f2
Benjamin Miller <benmiller179@gmail.com>
2024-09-17 19:04:26 -0500

    More colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,16 @@ module.exports = {
 				primary: {
 					25: '#FFF5F2',
 					50: '#FFEAE5',
+					100: '#FFD6CC',     // Light pastel salmon
+					200: '#FFBFAF',     // Soft pastel salmon
+					300: '#FFA699',     // Mild pastel salmon
+					400: '#FF8E82',     // Strong pastel salmon
+					500: '#FF9B7E',     // Default pastel salmon (as defined)
+					600: '#FF8269',     // Slightly darker than default
+					700: '#FF715A',     // Darker salmon (as defined)
+					800: '#D85B48',     // Dark salmon shade
+					900: '#B34638',     // Even darker salmon
+					950: '#8A3429',     // Darkest salmon shade, near brown
 					light: '#FFC4B2',   // Light pastel salmon
 					DEFAULT: '#FF9B7E', // Default pastel salmon
 					dark: '#FF715A',    // Darker salmon
