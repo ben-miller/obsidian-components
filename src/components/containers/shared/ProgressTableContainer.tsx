9eba42d497af46a392bdeac18e264a267007a471
Benjamin Miller <benmiller179@gmail.com>
2024-10-09 16:20:33 -0500

    Clean up imports

--- src/components/containers/shared/ProgressTableContainer.tsx
+++ src/components/containers/shared/ProgressTableContainer.tsx
@@ -1,5 +1,5 @@
 import {DocumentNode, useQuery} from "@apollo/client";
-import React, {useState} from "react";
+import React from "react";
 import styled from "styled-components";
 import {ProgressTable} from "../../presenters/ProgressTable/ProgressTable";
 import {ProgressTableData} from "../../../models/progressTableData";
